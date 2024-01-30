30ff79017f357b92d4410c23884067e6e5452b0a
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-01-30 10:31:38 +0800

    added drawer
    added drawer in navigation on mobile viewport

--- src/components/ui/navbar/index.tsx
+++ src/components/ui/navbar/index.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import UserMenu from "./user-menu";
 import WatchListifyLogo from "./logo";
+import Drawer from "./navbar-drawer";
 
 import { Search as SearchIcon, Library, Menu } from "lucide-react";
 
 const Navbar = () => {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const handleToggleDrawer = () => {
+    setDrawerOpen(!isDrawerOpen);
+  };
   return (
     <div className="flex justify-between content-center px-2 md:px-16 py-2 bg-slate-800">
       <div className="flex items-center space-x-8">
         <div className="flex items-center space-x-5 md:space-x-3">
-          <Menu className="block md:hidden" />
+          <Menu
+            className="block md:hidden cursor-pointer"
+            onClick={handleToggleDrawer}
+            id="drawer-button"
+            aria-controls="drawer"
+            aria-expanded="false"
+          />
+
+          <Drawer isOpen={isDrawerOpen} onClose={() => setDrawerOpen(false)} />
+
+          {/* App logo */}
           <WatchListifyLogo />
           <h1 className="text-xl font-semibold hidden md:block">
             WatchListify
           </h1>
         </div>
+
+        {/* Links */}
         <div className="hidden md:flex items-center space-x-4">
           <Link className="text-md lg:text-lg" href="/">
             Anime
