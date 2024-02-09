d5c142f7c4c6971fcae06ad5052e904977365e44
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-02-09 12:50:00 +0800

    added recent data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import { Button } from "@/components/ui/button";
+import axios from "axios";
 
-export default function Home() {
-  return <></>;
+import HomeHeader from "@/components/islets/home-header";
+import HomeRecentAnime from "@/components/islets/home-recent-anime";
+
+import { RecentAnimeTypes } from "@/lib/types";
+
+const getData = async (): Promise<{ recentAnime: RecentAnimeTypes[] }> => {
+  const { data } = await axios.get(
+    "https://api-consumet-raffy.vercel.app/anime/gogoanime/recent-episodes"
+  );
+
+  return { recentAnime: data.results };
+};
+
+export default async function Home() {
+  const { recentAnime } = await getData();
+  return (
+    <div className="px-16">
+      <HomeHeader />
+
+      {/* Recently Updated */}
+      <HomeRecentAnime recentAnime={recentAnime} />
+    </div>
+  );
 }
