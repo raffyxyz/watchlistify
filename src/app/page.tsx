44735e24ed762ae6e5d5daf361eb738a6092dc06
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-02-10 17:51:13 +0800

    update home page
    added recent, history, top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,45 @@ import axios from "axios";
 
 import HomeHeader from "@/components/islets/home-header";
 import HomeRecentAnime from "@/components/islets/home-recent-anime";
+import HomeWatchHistory from "@/components/islets/home-watch-history";
+import TopAiringAnime from "@/components/islets/home-top-anime";
 
-import { RecentAnimeTypes } from "@/lib/types";
+import { RecentAnimeTypes, TopAnimeTypes } from "@/lib/types";
 
-const getData = async (): Promise<{ recentAnime: RecentAnimeTypes[] }> => {
-  const { data } = await axios.get(
+import Footer from "@/components/islets/footer";
+
+const getData = async (): Promise<{
+  recentAnime: RecentAnimeTypes[];
+  topAnime: TopAnimeTypes[];
+}> => {
+  const { data: dataRecent } = await axios.get(
     "https://api-consumet-raffy.vercel.app/anime/gogoanime/recent-episodes"
   );
 
-  return { recentAnime: data.results };
+  const { data: dataTop } = await axios.get(
+    "https://api-consumet-raffy.vercel.app/anime/gogoanime/top-airing"
+  );
+
+  return { recentAnime: dataRecent.results, topAnime: dataTop.results };
 };
 
 export default async function Home() {
-  const { recentAnime } = await getData();
+  const { recentAnime, topAnime } = await getData();
+
   return (
-    <div className="px-16">
+    <div className="px-4 md:px-10 lg:px-16">
       <HomeHeader />
 
       {/* Recently Updated */}
       <HomeRecentAnime recentAnime={recentAnime} />
+
+      {/* Watch History */}
+      <HomeWatchHistory data={topAnime} />
+
+      {/* Top Airing Anime */}
+      <TopAiringAnime data={topAnime} />
+
+      <Footer />
     </div>
   );
 }
