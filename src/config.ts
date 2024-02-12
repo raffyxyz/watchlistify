ce590ed1df110d687abe1d736f589e5cffd9071a
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-02-12 09:48:04 +0800

    added api config

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const API_HOST = process.env.API_URL;
+export const API_HOST_CLIENT = process.env.NEXT_PUBLIC_API_URL;
+
+export const GOGOANIME_ENDPOINT = "/gogoanime";
+
+export const ANIME = "/anime";
