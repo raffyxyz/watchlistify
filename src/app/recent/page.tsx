a7742c3d8786ef603653e36360c81873d5c99f31
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-02-26 16:47:44 +0800

    updated recent page

--- src/app/recent/page.tsx
+++ src/app/recent/page.tsx
@@ -1,23 +1,9 @@
-import axios from "axios";
 import RecentUpdates from "@/components/islets/recent-updates";
 
-import { RecentAnimeTypes } from "@/lib/types";
-import { API_HOST, GOGOANIME_ENDPOINT, ANIME } from "@/config";
-
-const getData = async (): Promise<{ recentAnime: RecentAnimeTypes[] }> => {
-  const { data } = await axios.get(
-    `${API_HOST + ANIME + GOGOANIME_ENDPOINT}/recent-episodes`
-  );
-
-  return { recentAnime: data.results };
-};
-
-export default async function Recent() {
-  const { recentAnime } = await getData();
-
+export default function Recent() {
   return (
     <div className="px-4 md:px-10 lg:px-16">
-      <RecentUpdates recentAnime={recentAnime} />
+      <RecentUpdates />
     </div>
   );
 }
