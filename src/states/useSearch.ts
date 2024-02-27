ea3765c0a6d379c9a56230d203d4056573e3a8fe
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-02-27 23:50:08 +0800

    added useSearch state

---
+++ src/states/useSearch.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { createWithEqualityFn } from "zustand/traditional";
+import { shallow } from "zustand/shallow";
+import { API_HOST_CLIENT, ANIME, GOGOANIME_ENDPOINT } from "@/config";
+import { AnimeResultRequestTypes } from "@/lib/types";
+
+type State = {
+  searchQuery: string;
+  animePage: number;
+};
+
+type Action = {
+  setSearchQuery: (query: string) => void;
+  searchAnime: (
+    query: string,
+    page?: number
+  ) => Promise<AnimeResultRequestTypes>;
+};
+
+const initialState: State = {
+  searchQuery: "",
+  animePage: 1,
+};
+
+export const useSearch = createWithEqualityFn<State & Action>()((set, get) => ({
+  ...initialState,
+  setSearchQuery: (query: string) => set({ searchQuery: query }),
+  searchAnime: async (query: string, page?: number) => {
+    const { data } = await axios.get(
+      `${API_HOST_CLIENT + ANIME + GOGOANIME_ENDPOINT}/${query}`,
+      { params: { page } }
+    );
+
+    return data;
+  },
+  shallow,
+}));
