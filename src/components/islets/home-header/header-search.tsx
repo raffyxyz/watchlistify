4d5b403e32e2cd92b33a18b61e1a5fafa24fa19a
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-02-27 23:51:55 +0800

    updated header search

--- src/components/islets/home-header/header-search.tsx
+++ src/components/islets/home-header/header-search.tsx
@@ -1,8 +1,16 @@
+"use client";
 import React from "react";
 import { SearchInput } from "@/components/ui/search-input";
 import { SearchIcon } from "lucide-react";
 
+import { useSearch } from "@/states/useSearch";
+
 const HeaderSearch = () => {
+  const [searchQuery, setSearchQuery] = useSearch((state) => [
+    state.searchQuery,
+    state.setSearchQuery,
+  ]);
+
   return (
     <div className="w-full lg:w-[900px] m-auto mt-4">
       <SearchInput
@@ -10,6 +18,8 @@ const HeaderSearch = () => {
         type="text"
         className="h-12 md:h-14 text-md"
         placeholder="Search anime or kdrama"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
       />
     </div>
   );
