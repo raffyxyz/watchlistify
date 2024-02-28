9609bb36492fac50688045c2d31f7ec34cd53166
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-02-28 18:05:51 +0800

    added drama endpoint

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,8 @@ export const API_HOST_CLIENT = process.env.NEXT_PUBLIC_API_URL;
 
 export const GOGOANIME_ENDPOINT = "/gogoanime";
 
+export const DRAMA_COOL = "/dramacool";
+
 export const ANIME = "/anime";
+
+export const MOVIE = "/movies";
