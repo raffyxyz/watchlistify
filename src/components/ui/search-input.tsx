d893676c81c2e077e46c7a77c138f8fefb6de60e
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-01 18:05:50 +0800

    updated search

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -7,14 +7,15 @@ export interface SearchProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   icon: React.ReactNode;
   loading?: boolean;
+  filter?: JSX.Element;
 }
 
 const SearchInput = React.forwardRef<HTMLInputElement, SearchProps>(
-  ({ className, icon, loading, ...props }, ref) => {
+  ({ className, icon, loading, filter, ...props }, ref) => {
     return (
       <div
         className={cn(
-          "flex h-10 items-center rounded-md bg-slate-800 border border-input pl-3 pr-1 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-1",
+          "flex h-10 items-center rounded-md bg-slate-800 border border-input pl-3 pr-2 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-1",
           className
         )}
       >
@@ -25,6 +26,7 @@ const SearchInput = React.forwardRef<HTMLInputElement, SearchProps>(
           ref={ref}
           className="w-full p-2 bg-slate-800 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
         />
+        {filter}
       </div>
     );
   }
