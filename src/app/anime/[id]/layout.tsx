b541357d8934d9bec2e990e87897fe4654ea38d0
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-12 09:51:49 +0800

    added a loader fallback

--- src/app/anime/[id]/layout.tsx
+++ src/app/anime/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
+import Loader from "@/components/ui/loader";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Suspense>{children}</Suspense>;
+  return <Suspense fallback={<Loader />}>{children}</Suspense>;
 }
