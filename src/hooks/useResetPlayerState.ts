8323250a30bbda055ebd3d4615a032b359f77e88
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-13 00:35:21 +0800

    cleanup

--- src/hooks/useResetPlayerState.ts
+++ src/hooks/useResetPlayerState.ts
@@ -1,16 +1,13 @@
 import { useEffect } from "react";
-import { useAnimeEpisode } from "@/states/useAnimeEpisode";
 import { useAnimeEpisodeChange } from "@/states/useAnimeEpisodeChange";
 
 function useResetPlayerState() {
   const setCurrentIndex = useAnimeEpisodeChange(
     (state) => state.setCurrentIndex
   );
-  const [setQuality] = useAnimeEpisode((state) => [state.setQuality]);
 
   const reset = () => {
     setCurrentIndex(1);
-    setQuality("default");
   };
 
   useEffect(() => {
