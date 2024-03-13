b8afbd3be8912dd150d81923d99fdf60dd265a57
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-13 09:43:58 +0800

    cleanup

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import axios from "axios";
 
-import { AnimeInfo, EpisodeType } from "@/lib/types";
+import { AnimeInfo } from "@/lib/types";
 import { API_HOST, GOGOANIME_ENDPOINT, ANIME } from "../../../config";
-import { Button } from "@/components/ui/button";
-import {
-  AnimePlayer,
-  AnimeWrapper,
-  AnimeDetails,
-  AnimeActions,
-} from "@/components/islets/anime-islets";
-import AnimeEpisodes from "@/components/islets/anime-islets/anime-episodes";
 
 const getData = async (id: string): Promise<{ animeInfo: AnimeInfo }> => {
   const { data } = await axios.get(
@@ -29,23 +21,7 @@ export default async function AnimePage({
 
   return (
     <>
-      {/* Video component */}
-      <AnimePlayer
-        episodeId={animeInfo?.episodes[0]?.id}
-        cover={animeInfo.image}
-      />
-      <AnimeActions id={animeInfo.episodes[0]?.id} />
-
-      <AnimeWrapper>
-        <AnimeDetails
-          title={animeInfo.title}
-          subOrDub={animeInfo.subOrDub}
-          status={animeInfo.status}
-          description={animeInfo.description}
-        />
-
-        <AnimeEpisodes animeEpisodes={animeInfo.episodes} />
-      </AnimeWrapper>
+      <h1>Anime Info: {animeInfo.title}</h1>
     </>
   );
 }
