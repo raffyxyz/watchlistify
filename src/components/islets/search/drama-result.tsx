f5bef5fdf15e475facf725b0035eaea3e4a50920
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-13 21:53:09 +0800

    updated links on search results

--- src/components/islets/search/drama-result.tsx
+++ src/components/islets/search/drama-result.tsx
@@ -22,6 +22,9 @@ const DramaResult: React.FC<DramaResultProps> = ({ data }) => {
             className="w-[190px] sm:w-[200px] md:w-[190px] lg:w-[200px] xl:w-[240px] 2xl:w-[280px] h-[260px] sm:h-[260px] md:h-[240px] lg:h-[270px] xl:h-[300px] 2xl:h-[390px] hover:scale-105"
             src={drama.image}
             alt={drama.title}
+            onClick={() =>
+              router.push(`/drama/${encodeURIComponent(drama.id)}`)
+            }
           />
           <div className="mt-2 flex justify-between items-center">
             <div className="flex space-x-1">
@@ -32,11 +35,21 @@ const DramaResult: React.FC<DramaResultProps> = ({ data }) => {
             </div>
 
             <div className="flex space-x-2">
-              <Play className="text-orange-400 w-[18px] md:w-[24px]" />
+              <Play
+                className="text-orange-400 w-[18px] md:w-[24px]"
+                onClick={() =>
+                  router.push(`/drama/${encodeURIComponent(drama.id)}`)
+                }
+              />
               <Plus className="text-orange-400  w-[22px] md:w-[26px]" />
             </div>
           </div>
-          <h3 className="mt-1 hover:text-orange-400">{drama.title}</h3>
+          <Link
+            href={`/drama/${encodeURIComponent(drama.id)}`}
+            className="mt-1 hover:text-orange-400"
+          >
+            {drama.title}
+          </Link>
         </div>
       ))}
     </div>
