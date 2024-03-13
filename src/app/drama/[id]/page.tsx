1bfb14f35e6aa128c6248c91c12a3f52f8144d49
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-13 21:52:51 +0800

    added drama info page

---
+++ src/app/drama/[id]/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+import {
+  DramaInfoWrapper,
+  DramaInfo,
+  DramaInfoPlus,
+} from "@/components/islets/drama-info-islets";
+import { API_HOST, MOVIE, DRAMA_COOL } from "@/config";
+import { DramaDetailsType } from "@/lib/types";
+
+const getData = async (
+  id: string
+): Promise<{ dramaInfo: DramaDetailsType }> => {
+  const { data } = await axios.get(`${API_HOST + MOVIE + DRAMA_COOL}/info`, {
+    params: { id: encodeURIComponent(id) },
+  });
+
+  return { dramaInfo: data };
+};
+
+export default async function Drama({ params }: { params: { id: string } }) {
+  const { dramaInfo } = await getData(params.id);
+  return (
+    <DramaInfoWrapper background={dramaInfo.image}>
+      <DramaInfo dramaInfo={dramaInfo} />
+      <DramaInfoPlus dramaInfo={dramaInfo} />
+    </DramaInfoWrapper>
+  );
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const { dramaInfo } = await getData(params.id);
+  return {
+    title: dramaInfo.title,
+  };
+}
---
+++ src/components/islets/drama-info-islets/drama-info.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Calendar, Globe, Play, Bookmark } from "lucide-react";
+
+import { DramaDetailsType } from "@/lib/types";
+
+interface DramaInfoProps {
+  dramaInfo: DramaDetailsType;
+}
+
+const DramaInfo: React.FC<DramaInfoProps> = ({ dramaInfo }) => {
+  const router = useRouter();
+
+  return (
+    <div className="col-span-2">
+      <div className="flex flex-col">
+        <img
+          className="w-[190px] sm:w-[200px] md:w-[190px] lg:w-[200px] xl:w-[240px] 2xl:w-[280px] h-[260px] sm:h-[260px] md:h-[240px] lg:h-[270px] xl:h-[300px] 2xl:h-[390px]"
+          src={dramaInfo.image}
+          alt={dramaInfo.title}
+        />
+        <h1 className="mt-4 text-lg font-semibold text-orange-400">
+          {dramaInfo.title}
+        </h1>
+        <div className=" flex space-x-3 items-center">
+          {/* <div className="flex items-center space-x-2">
+            <Play size={16} />
+            <span className="font-sm lowercase">{dramaInfo.type}</span>
+          </div>
+          <div className="flex items-center space-x-2">
+            <Globe size={16} />
+            <span className="font-sm">{dramaInfo.duration}</span>
+          </div> */}
+          <div className="flex items-center space-x-2">
+            <Calendar size={16} />
+            <span className="font-sm">{dramaInfo.releaseDate}</span>
+          </div>
+        </div>
+        <div className="mt-4 flex space-x-3 items-center">
+          <Button
+            variant="orange"
+            onClick={() => router.push(`/anime/watch/${dramaInfo.id}`)}
+          >
+            <Play className="mr-2 h-4 w-4" strokeWidth="3px" />
+            Watch Now
+          </Button>
+          <Button variant="secondary">
+            <Bookmark className="mr-2 h-4 w-4" strokeWidth="3px" />
+            Add to Library
+          </Button>
+        </div>
+        <p className="mt-4">{dramaInfo.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DramaInfo;
