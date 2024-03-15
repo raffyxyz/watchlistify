dcda2dad1ac57452285789b9696b9c02ccf34b72
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-15 22:22:00 +0800

    added create watchlist server action

---
+++ src/app/actions/create-watchlist.ts
@@ -0,0 +1,29 @@
+"use server";
+import { getServerSession } from "next-auth";
+import connectMongo from "@/utils/mongoose";
+import WatchList from "@/models/watchlist";
+
+export async function createWatchList(prevState: any, formData: FormData) {
+  const session = await getServerSession();
+
+  if (!session) {
+    return { message: "login" };
+  }
+
+  try {
+    await connectMongo();
+    await WatchList.create({
+      userId: session?.user?.email,
+      listId: formData.get("listId"),
+      title: formData.get("title"),
+      image: formData.get("image"),
+      type: formData.get("type"),
+      status: formData.get("status"),
+    });
+
+    return { message: "success" };
+  } catch (error) {
+    console.log(error);
+    return { message: error };
+  }
+}
