7c6e4648c9029bd2b8ded02ae6d499c8dafee9e7
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-17 18:44:58 +0800

    updated watchlist

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,33 +1,10 @@
-import React, { cache } from "react";
-import { getServerSession } from "next-auth";
-import connectMongo from "@/utils/mongoose";
-import WatchList from "@/models/watchlist";
+import React from "react";
 import WatchListData from "@/components/islets/watchlist-islets";
 
-import { WatchListType } from "@/lib/types";
-
-const getData = cache(async (): Promise<{ watchList: WatchListType[] }> => {
-  const session = await getServerSession();
-
-  await connectMongo();
-
-  const result = await WatchList.find({
-    userId: session?.user?.email,
-  }).exec();
-
-  const watchList = JSON.parse(JSON.stringify(result));
-
-  return { watchList };
-});
-
-export default async function Library() {
-  const { watchList } = await getData();
-
+export default async function WatchList() {
   return (
     <div className="px-4 md:px-10 lg:px-16 xs:m-auto sm:m-0 xs:w-[450px] sm:w-full">
-      <WatchListData data={watchList} />
+      <WatchListData />
     </div>
   );
 }
-
-export const revalidate = 0;
