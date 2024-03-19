9511c4fac50d34a4339755fe59accb4c392a755d
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-19 13:00:45 +0800

    added search params

--- src/lib/watchList.ts
+++ src/lib/watchList.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import { WatchListType } from "@/lib/types";
 
-export const fetchUserWatchList = async (): Promise<WatchListType[]> => {
+export const fetchUserWatchList = async (
+  status?: string,
+  type?: string | undefined
+): Promise<WatchListType[]> => {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}api/watchlist`
+    `${
+      process.env.NEXT_PUBLIC_NEXTAUTH_URL
+    }api/watchlist?status=${status}&type=${type?.toLowerCase()}`
   );
   return data;
 };
