4964dc4c93fb531960823a4499d78071a4d1ab57
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-20 16:07:35 +0800

    add dynamic url decoding

--- src/lib/watchList.ts
+++ src/lib/watchList.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { WatchListType } from "@/lib/types";
+import { isEncoded } from "./utils";
 
 export const fetchUserWatchList = async (
   status?: string,
@@ -17,7 +18,9 @@ export const getUserWatchListInfo = async (
   listId: string
 ): Promise<WatchListType> => {
   const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_APP_URL}/api/watchlist/${listId}`
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/watchlist/${
+      isEncoded(listId) ? listId : encodeURIComponent(listId)
+    }`
   );
 
   return data;
