6bdfbd080264c72db72cda3f842546729de302bf
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-20 12:38:10 +0800

    added new util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function encodeUrlPart(url: string) {
+  const prefixIndex = url.indexOf("/", url.indexOf("/") + 1) + 1; // Find second '/'
+  const partToEncode = url.substring(prefixIndex);
+  return url.substring(0, prefixIndex) + encodeURIComponent(partToEncode);
 }
