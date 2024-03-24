115f337e0fb18fb196588774bf34b64fa43bdf04
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-24 12:48:59 +0800

    chore: refactor

--- src/components/islets/drama-watch-islets/drama-episodes.tsx
+++ src/components/islets/drama-watch-islets/drama-episodes.tsx
@@ -1,17 +1,16 @@
 "use client";
 import React, { useEffect } from "react";
-import axios from "axios";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useDramaEpisode } from "@/states/useDramaEpisode";
 import { useDramaEpisodeChange } from "@/states/useDramaEpisodeChange";
 import { Button } from "@/components/ui/button";
 import { DramaEpisodeType } from "@/lib/types";
 import DramaEpisodeChange from "./drama-episode-select";
+import { updateUserWatchList } from "@/lib/watchList";
 
 interface DramaEpisodesProps {
-  dramaEpisodes: Array<DramaEpisodeType> | undefined;
+  dramaEpisodes: DramaEpisodeType[] | undefined;
   id: string;
   isWatchList: boolean;
 }
@@ -21,16 +20,14 @@ const DramaEpisodes: React.FC<DramaEpisodesProps> = ({
   id,
   isWatchList,
 }) => {
-  const queryClient = useQueryClient();
   const router = useRouter();
   const searchParams = useSearchParams();
-  const dramaEpisodeParams = searchParams.get("dEp") as string;
+  const dramaEpisodeParams = searchParams.get("dEp") || "";
 
   const [setDramaEpisode, selectedDramaEpisode] = useDramaEpisode((state) => [
     state.setDramaEpisode,
     state.dramaEpisode,
   ]);
-
   const [startIndexDrama, endIndexDrama] = useDramaEpisodeChange((state) => [
     state.startIndexDrama,
     state.endIndexDrama,
@@ -42,78 +39,60 @@ const DramaEpisodes: React.FC<DramaEpisodesProps> = ({
   );
 
   const updateMutation = useMutation({
-    mutationFn: async (data: {
-      episode: number;
-      episodeId: string;
-      listId: string;
-    }) => {
-      const { episode, episodeId, listId } = data;
-      return axios.put(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/watchlist/${encodeURIComponent(
-          listId
-        )}`,
-        { episode, episodeId }
-      );
-    },
+    mutationFn: updateUserWatchList,
   });
 
   const handleEpisodeClick = (episode: string, epNumber: number) => {
-    setDramaEpisode(episode);
-
-    if (isWatchList) {
+    if (episode) {
+      setDramaEpisode(episode);
+    }
+    if (isWatchList && episode) {
       updateMutation.mutate({
         episode: epNumber,
         episodeId: episode,
         listId: id,
       });
     }
-
-    // queryClient.invalidateQueries({
-    //   queryKey: ["streamingLinksDrama", selectedDramaEpisode, id],
-    // });
-
-    router.push(`?dEp=${episode}`);
+    if (episode) {
+      router.push(`?dEp=${episode}`);
+    }
   };
 
   useEffect(() => {
-    if (dramaEpisodeParams) {
-      setDramaEpisode(dramaEpisodeParams);
-    } else if (dramaEpisodes && dramaEpisodes[0]) {
-      setDramaEpisode(dramaEpisodes[0]?.id);
+    const selectedDramaEpisodeId = dramaEpisodeParams || dramaEpisodes?.[0]?.id;
+    if (selectedDramaEpisodeId) {
+      setDramaEpisode(selectedDramaEpisodeId);
     }
 
     if (dramaEpisodeParams && isWatchList) {
       const parts = dramaEpisodeParams.split("-");
       const episodeNumber = parseInt(parts[parts.length - 1]);
-
       updateMutation.mutate({
         episode: episodeNumber,
         episodeId: dramaEpisodeParams,
         listId: id,
       });
     }
-  }, [dramaEpisodeParams]);
+  }, [dramaEpisodeParams, dramaEpisodes, isWatchList, id]);
 
-  if (dramaEpisodes?.length === 0) {
+  if (!dramaEpisodes?.length) {
     return <div className="mt-10 w-full">No episodes found.</div>;
   }
 
   return (
     <div className="w-full">
       <div className="flex justify-between items-center">
-        <h1 className="mb-1">Episodes: </h1>
-        <DramaEpisodeChange
-          totalEpisodes={dramaEpisodes ? dramaEpisodes?.length : 0}
-        />
+        <h1 className="mb-1">Episodes:</h1>
+        <DramaEpisodeChange totalEpisodes={dramaEpisodes?.length || 0} />
       </div>
       <div className="flex flex-wrap">
-        {dramaEpisodesList?.map((episode: DramaEpisodeType) => (
+        {dramaEpisodesList?.map((episode) => (
           <Button
             key={episode.id}
             className="mr-2 mt-2"
-            variant={`${
+            variant={
               selectedDramaEpisode === episode.id ? "orange" : "secondary"
-            }`}
+            }
             onClick={() => handleEpisodeClick(episode.id, episode.episode)}
           >
             {episode.episode}
