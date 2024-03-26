489890e2205c34f9aace9b98313d5fa640cfe39f
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-26 18:53:04 +0800

    chore: added fetchAnimeInfo

--- src/lib/anime.ts
+++ src/lib/anime.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { AnimeVideoDetailsType } from "@/lib/types";
+import { AnimeVideoDetailsType, AnimeInfo } from "@/lib/types";
 import { API_HOST_CLIENT, GOGOANIME_ENDPOINT, ANIME } from "@/config";
 
 export const fetchAnimeStreamingLinks = async (
@@ -8,6 +8,16 @@ export const fetchAnimeStreamingLinks = async (
   const { data } = await axios.get(
     `${API_HOST_CLIENT + ANIME + GOGOANIME_ENDPOINT}/watch/${encodeURIComponent(
       episode
+    )}`
+  );
+
+  return data;
+};
+
+export const fetchAnimeInfo = async (id: string): Promise<AnimeInfo> => {
+  const { data } = await axios.get(
+    `${API_HOST_CLIENT + ANIME + GOGOANIME_ENDPOINT}/info/${encodeURIComponent(
+      id
     )}`
   );
 
