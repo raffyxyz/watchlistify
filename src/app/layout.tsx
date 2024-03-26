daf529857c787e57f50f65fd0e3558cc5c60e9f2
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-03-26 18:50:53 +0800

    chore: added getServerSession

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,48 @@
 import type { Metadata } from "next";
-import { Lato } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
+import { getServerSession } from "next-auth";
 import ReactQueryWrapper from "@/components/react-query-wrapper";
 import MainLayout from "@/components/layouts/main-layout";
 import SessionWrapper from "@/components/SessionWrapper";
 import GoogleAnalytics from "@/components/google-analytics";
+import { Toaster } from "@/components/ui/toaster";
+import { Lato } from "next/font/google";
+import "./globals.css";
 
 const lato = Lato({
   subsets: ["latin"],
   weight: "400",
 });
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await getServerSession();
+  return (
+    <SessionWrapper>
+      <html lang="en">
+        <body className={lato.className}>
+          <GoogleAnalytics />
+          <ReactQueryWrapper>
+            <MainLayout
+              user={
+                session?.user as {
+                  name: string;
+                  email: string;
+                  image: string;
+                }
+              }
+            >
+              {children}
+            </MainLayout>
+          </ReactQueryWrapper>
+          <Toaster />
+        </body>
+      </html>
+    </SessionWrapper>
+  );
+}
 
 export const metadata: Metadata = {
   title: { default: "WatchListify", template: "%s | WatchListify" },
@@ -32,23 +64,3 @@ export const metadata: Metadata = {
     },
   ],
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <SessionWrapper>
-      <html lang="en">
-        <body className={lato.className}>
-          <GoogleAnalytics />
-          <ReactQueryWrapper>
-            <MainLayout>{children}</MainLayout>
-          </ReactQueryWrapper>
-          <Toaster />
-        </body>
-      </html>
-    </SessionWrapper>
-  );
-}
