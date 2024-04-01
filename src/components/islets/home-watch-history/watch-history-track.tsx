e9aa93656f29d8eeaf092eeebd6c792f688d7046
raffyxyz <raffy.amoguis1@gmail.com>
2024-04-01 13:14:14 +0800

    chore: cleanup

--- src/components/islets/home-watch-history/watch-history-track.tsx
+++ src/components/islets/home-watch-history/watch-history-track.tsx
@@ -1,114 +1,19 @@
 "use client";
 import React from "react";
-import Link from "next/link";
-
-import { Img } from "@/components/ui/img";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { WatchListType } from "@/lib/types";
-import { fetchUserWatchList } from "@/lib/watchList";
+import { WatchHistoryMobileView } from "./watch-history-mobile";
+import { WatchHistoryDefaultView } from "./watch-history-default";
 
 const WatchHistoryTrack: React.FC<{
   userWatchList: WatchListType[] | undefined;
 }> = ({ userWatchList }) => {
   return (
     <>
-      <div className="hidden md:block">
-        <div className="mt-4 grid md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 xl:gap-5">
-          {userWatchList?.slice(0, 6).map((watchList: WatchListType) => (
-            <div key={watchList._id} className="cursor-pointer">
-              <Link
-                href={`${
-                  watchList.type === "anime"
-                    ? "anime/watch/" +
-                      watchList.listId +
-                      "?ep=" +
-                      watchList.episodeId
-                    : "drama/watch/" +
-                      encodeURIComponent(watchList.listId) +
-                      "?dEp=" +
-                      watchList.episodeId
-                }`}
-                className="mt-3 hover:text-orange-400"
-              >
-                <Img
-                  className="w-[280px] h-[200px] object-cover"
-                  src={watchList.image}
-                  alt={watchList.title}
-                />
-              </Link>
-              <h3 className="text-xs text-muted-foreground">
-                Episode {watchList.episode ?? 0}
-              </h3>
-              <Link
-                href={`${
-                  watchList.type === "anime"
-                    ? "anime/watch/" +
-                      watchList.listId +
-                      "?ep=" +
-                      watchList.episodeId
-                    : "drama/watch/" +
-                      encodeURIComponent(watchList.listId) +
-                      "?dEp=" +
-                      watchList.episodeId
-                }`}
-                className="mt-3 hover:text-orange-400 line-clamp-2"
-              >
-                {watchList.title}
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
+      {/* Large screen view. */}
+      <WatchHistoryDefaultView userWatchList={userWatchList} />
 
-      <div className="block md:hidden">
-        <ScrollArea className="mt-4 w-full whitespace-nowrap">
-          <div className="flex w-max space-x-2">
-            {userWatchList?.slice(0, 12).map((watchList: WatchListType) => (
-              <div key={watchList._id} className="cursor-pointer">
-                <Link
-                  href={`${
-                    watchList.type === "anime"
-                      ? "anime/watch/" +
-                        watchList.listId +
-                        "?ep=" +
-                        watchList.episodeId
-                      : "drama/watch/" +
-                        encodeURIComponent(watchList.listId) +
-                        "?dEp=" +
-                        watchList.episodeId
-                  }`}
-                >
-                  <Img
-                    className="w-[200px] h-[190px] object-cover"
-                    src={watchList.image}
-                    alt={watchList.title}
-                  />
-                </Link>
-                <h3 className="text-xs text-muted-foreground">
-                  Episode {watchList.episode ?? 0}
-                </h3>
-                <Link
-                  href={`${
-                    watchList.type === "anime"
-                      ? "anime/watch/" +
-                        watchList.listId +
-                        "?ep=" +
-                        watchList.episodeId
-                      : "drama/watch/" +
-                        encodeURIComponent(watchList.listId) +
-                        "?dEp=" +
-                        watchList.episodeId
-                  }`}
-                  className="mt-3 hover:text-orange-400 line-clamp-2"
-                >
-                  {watchList.title}
-                </Link>
-              </div>
-            ))}
-          </div>
-          <ScrollBar orientation="horizontal" />
-        </ScrollArea>
-      </div>
+      {/* Mobile View */}
+      <WatchHistoryMobileView userWatchList={userWatchList} />
     </>
   );
 };
