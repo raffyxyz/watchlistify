c8aff375e1c60f19b4232d2d10375e3226d7e46c
raffyxyz <raffy.amoguis1@gmail.com>
2024-06-15 09:53:29 +0800

    chore: updated mongoose initiation

--- src/utils/mongoose.ts
+++ src/utils/mongoose.ts
@@ -2,7 +2,18 @@ import mongoose from "mongoose";
 
 import { MONGO_URI, DATABASE } from "@/config";
 
-const connectMongo = async () =>
-  mongoose.connect(MONGO_URI as string, { dbName: DATABASE });
+if (!MONGO_URI) throw new Error("MONGO_URI is not defined.");
+
+let cached = global.mongoose;
+
+if (!cached) cached = global.mongoose = { conn: null };
+
+const connectMongo = async () => {
+  if (cached.conn) return cached.conn;
+
+  cached.conn = await mongoose.connect(MONGO_URI, { dbName: DATABASE });
+
+  return cached.conn;
+};
 
 export default connectMongo;
