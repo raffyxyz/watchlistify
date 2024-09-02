b53c348625d68810cf18a94755358e77f599f97c
Raffy Amoguis <raffy.amoguis1@gmail.com>
2024-09-02 09:06:37 +0800

    chore: added switch for pwa

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 export default withPWA(nextConfig);
